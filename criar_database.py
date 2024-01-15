3ab92f67e17548d21af7e4299abaecb0c48a5099
David Shelton <79729565+DavidSheltonSF@users.noreply.github.com>
2024-01-15 14:19:15 -0300

    Delete criar_database.py

--- criar_database.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-data_base = [{"username": "David", "lastname": "Shelton", "userpassword": "david123", "userbirthday": "26/02/2002", "usergender": "masculino"}]
-with open("database.pickle", "wb") as db:
-    pickle.dump(data_base, db)
